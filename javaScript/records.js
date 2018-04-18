569cc0be193e5e319a736e606d1f16594fed9f4c
William T. McCormick <wtmccormick@gmail.com>
2018-04-17 22:30:24 -0500

    Started new project

---
+++ javaScript/records.js
@@ -0,0 +1,59 @@
+var records = [
+    {
+        artist: "Herbie Hancock",
+        album: "Sunlight",
+        year: 1978,
+        label: "Columbia",
+        genre: "Jazz",
+        condition: "VG+"
+    },
+    {
+        artist: "Betty Wright",
+        album: "Hard to Stop",
+        year: 1973,
+        label: "Alston Records",
+        genre: "Soul",
+        condition: "VG"
+    },
+    {
+        artist: "Jerry Moore",
+        album: "Life is a Constant Journey Home",
+        year: 1967,
+        label: "ESP Disk",
+        genre: "Soul",
+        condition: "VG+"
+    },
+    {
+        artist: "Bill LaBounty",
+        album: "Bill LaBounty",
+        year: 1982,
+        label: "Curb Records",
+        genre: "Easy Glide",
+        condition: "NM"
+    },
+    {
+        artist: "Laraaji",
+        album: "Vision Songs, Vol. 1",
+        year: 2018,
+        label: "Numero Group",
+        genre: "New Age",
+        condition: "NM"
+    },
+    {
+        artist: "William Ackerman",
+        album: "It Takes A Year",
+        year: 1977,
+        label: "Windham Hill",
+        genre: "Guitar Soli",
+        condition: "NM"
+    },
+    {
+        artist: "The XX",
+        album: "I See You",
+        year: 2017,
+        label: "Young Turks",
+        genre: "Pop",
+        condition: "NM"
+    },
+
+]
